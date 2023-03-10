ec8133642b39699e1d8c69dbb3a9bb6e46f08f21
Charlie Holtz <charlieholtz@gmail.com>
2023-03-10 16:47:19 -0500

    Pull contributions

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# Replicate
+# Replicate API Raycast Extension
 
-Run replicate models
+View your prediction history and search for predictions by prompt.
+
+Create an account and get an API token here (required): https://replicate.com
+
+If you attempt to copy an image to the clipboard, you will be asked by the system for permission.
+
+Source repo: https://github.com/KevinBatdorf/replicate-raycast
+
+## Features
+
+- Search your prompts
+- View predictions as a grid
+- View details about a prediction
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,24 @@
-enum Filter {
-  All = "all",
-  Open = "open",
-  Completed = "completed",
-}
-
-interface Todo {
+export type PredictionResponse = {
+  next: string | null;
+  previous: string | null;
+  results: Prediction[];
+  detail?: string;
+};
+export type Prediction = {
   id: string;
-  title: string;
-  isCompleted: boolean;
-}
-
-export { Filter };
-export type { Todo };
+  urls: {
+    get: string;
+  };
+  input: { prompt?: string } & Record<string, any>;
+  status: "starting" | "processing" | "succeeded" | "failed" | "cancelled";
+  output: string[] | string;
+  metrics: {
+    predict_time: number;
+  };
+};
+export type dbEntry = {
+  id: string;
+  src: string;
+  url: string;
+  prompt?: string;
+};
